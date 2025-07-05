7abaf7fe4765e2ba9d66b8972f6d41e33562f9a5
liantsoaaa <hei.liantsoa.2@gmail.com>
2025-07-05 07:35:48 +0300

    feat: Setting up all page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Home from './pages/Home';
+import About from './pages/About';
+import Programs from './pages/Programs';
+import Admission from './pages/Admission';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="programs" element={<Programs />} />
+          <Route path="admission" element={<Admission />} />
+        </Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import HeroSection from '../components/HeroSection';
+import AboutSection from '../components/AboutSection';
+import TeamSection from '../components/TeamSection';
+import ProgramsSection from '../components/ProgramsSection';
+
+export default function Home() {
+    return (
+        <div>
+            <HeroSection />
+            <AboutSection />
+            <TeamSection />
+            <ProgramsSection />
+        </div>
+    );
+}
